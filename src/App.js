1e3ffda10a54959d68d88d16cdda705399403cd8
rubzen <38146181+rubzen@users.noreply.github.com>
2018-09-15 00:17:05 -0500

    Ok

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { Route, Switch } from 'react-router-dom';
 
 import PortfolioFixed from './components/PortfolioFixed.js';
 import HomeView from './components/HomeView';
-import ProjectsView from './components/ProjectsView';
 import CvView from './components/CvView';
+import ProjectsView from "./components/ProjectsView";
 // b.2 Import the View Component Files
 
 import Nav from './components/Nav';
@@ -20,6 +20,17 @@ class App extends Component {
         <PortfolioFixed/>
 
         {/* Configure Switch + Route to render View Component files */}
+        <Switch>
+          <Route exact path="/" component={HomeView} />
+          <Route exact path="/CvView" component={props =>{
+            return <CvView {...props} />;
+          }}
+        />
+          <Route exact path="/ProjectsView" component={props =>{
+            return <ProjectsView {...props} />;
+          }}
+        />
+        </Switch>
 
       </div>
     );
