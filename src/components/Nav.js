6e9d671601630e03440050f6dea02936d77bfe46
rubzen <38146181+rubzen@users.noreply.github.com>
2018-09-15 01:05:44 -0500

    Ok

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 // c.1 Import + Configure the NavLink
-
+import { NavLink } from "react-router-dom";
 
 class Nav extends Component {
   render() {
     return (
-      <nav>
+      <nav ClassName='nav'>
         {/* c.2 instantiate nav links to '/', '/cv', '/projects' routes
             - navlink has activeClassName 'navitem--selected'
         */}
-
-        <a href="#">Route-1</a>
-        <a href="#">Route-2</a>
-        <a className='navitem--selected' href="#">Route-3</a>
+        <NavLink exact activeClassName="navitem--selected" to="/">Home</NavLink>
+        <NavLink activeClassName="navitem--selected" to="/cv">CV</NavLink>
+        <NavLink activeClassName="navitem--selected" to="/projects">Projects</NavLink>
       </nav>
     )
   }
